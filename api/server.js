491e4883b3ab0fcbd634b02bcf929aeeb2b82c13
danial.asaria <dasaria@usc.edu>
2022-05-06 17:18:53 -0700

    hi

--- api/server.js
+++ api/server.js
@@ -1,17 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
+// const __dirname = path.resolve();
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+mongoose.connect(process.env.MONGODB_URL, {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
+// console.log(process.env.MONGODB_URL)
 // Models
 const Todo = require('./models/Todo');
 
@@ -57,4 +61,15 @@ app.put('/todo/update/:id', async (req, res) => {
 	res.json(todo);
 });
 
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
+} else{
+	app.get("*", (req, res) => {
+		res.send("Api running");
+	});
+}
 app.listen(3001);
