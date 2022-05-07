4b46203bdf2ef7a48545e5e2058b651d08d28b34
danial.asaria <dasaria@usc.edu>
2022-05-06 17:43:13 -0700

    changed procfile

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 // const __dirname = path.resolve();
 const app = express();
 
@@ -61,15 +60,24 @@ app.put('/todo/update/:id', async (req, res) => {
 	res.json(todo);
 });
 
-if(process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
+// if(process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "client/build")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-	});
-} else{
-	app.get("*", (req, res) => {
-		res.send("Api running");
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// 	});
+// } else{
+// 	app.get("*", (req, res) => {
+// 		res.send("Api running");
+// 	});
+// }
+
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(3001);
